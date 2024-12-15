dae30d8cb9e0e4eb684249334e79174a9a552c22
om <awsdevopsom@gmail.com>
2024-12-15 13:18:36 +0530

    update

--- vars/vars.tf
+++ vars/vars.tf
@@ -16,3 +16,16 @@ output "sample2" {
     value = var.sample2[1] 
   
 }
+
+variable "map" {
+    default = {
+        apple = 100
+        banana = 200
+        orange = 10
+    }
+  
+}
+
+output "quantity" {
+  value = var.map["apple"]
+}
