f551db485dbdf3024e6867c225b9b9e221ec2633
om <awsdevopsom@gmail.com>
2024-12-17 22:01:15 +0530

    update

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,13 @@
 module "test"{
+  for_each = var.instance
   source = "./test"
 }
+
+variable "instance"{
+    default = {
+        frontend = {}
+        backend = {}
+        mysql = {}
+    }
+
+  }
