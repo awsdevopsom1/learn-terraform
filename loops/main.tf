3f9c55aad42abeeab62d7f94d911b8f3b3cc50e9
om <awsdevopsom@gmail.com>
2024-12-17 20:04:30 +0530

    update

--- loops/main.tf
+++ loops/main.tf
@@ -1,7 +1,30 @@
-
 variable "instances" {
   default = ["frontend", "backend", "mysql"]
 }
 resource "null_resource" "sample" {
      count = length(var.instances)
+}
+
+variable "instance1" {
+    default = {
+        frontend = {
+            name = frontend
+            instance_type = t3.micro
+        }
+    
+        backend = {
+            name = backendend
+            instance_type = t3.micro
+        }
+        mysql = {
+            name = mysql
+            instance_type = t3.micro
+        }    
+    }
+  
+}
+
+resource "null_resource" "sampe1" {
+    for_each = var.instance1
+  
 }
