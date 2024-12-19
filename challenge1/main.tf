55e9b603351f0ce9947c8ea39844ff0a8ed749b0
om <awsdevopsom@gmail.com>
2024-12-19 13:31:44 +0530

    update

--- challenge1/main.tf
+++ challenge1/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region =  "us-east-1"
 }
 resource "aws_instance" "challenge1" {
-    ami = "fdsojf5jt4j3"
+    ami = "ami-0b4f379183e5706b9"
     instance_type = "t2.micro"
    tags = {
      Name = "Terraforminstance"
