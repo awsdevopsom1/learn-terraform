7a6adf5a3b2aec4cb1055c3e719f3814d791ffc8
om <awsdevopsom@gmail.com>
2024-12-19 12:34:21 +0530

    update

--- wrap/main.tf
+++ wrap/main.tf
@@ -1,11 +1,24 @@
-resource "aws_vpc" "myvpc" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-          Name = var.inputname
-    }  
-}
+# resource "aws_vpc" "myvpc" {
+#     cidr_block = "10.0.0.0/16"
+#     tags = {
+#           Name = var.inputname
+#     }  
+# }
 
-variable "inputname" {
-    default = "vpc"
+# variable "inputname" {
+#     default = "vpc"
+  
+# }
+
+variable "mytuple" {
+    default = [cat, 1, dog]
+}
+variable "objects" {
+
+    default = {
+        name = om
+        height = 5.6
+
+    }
   
 }
